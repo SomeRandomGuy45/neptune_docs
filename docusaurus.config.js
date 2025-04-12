0bef31e27e80f5123b8e4e42fc3951571b4e41e6
SomeRandomGuy45 <dragonlionmusic@gmail.com>
2025-04-12 16:12:11 -0500

    asdad

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@
 
 import {themes as prismThemes} from 'prism-react-renderer';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...) a
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
